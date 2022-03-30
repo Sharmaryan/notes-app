fad94e73a814b4456881b735f330247b60150b7e
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-03-30 13:55:14 +0530

    feat : added note component

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import './Homepage.css'
 import SearchBar from "../../Components/SearchBar/SearchBar";
+import Note from "../../Components/Note/Note";
 const Homepage = () => {
   return (
     <div className="homepage">
     <SearchBar/>
+    <Note/>
+    <Note/>
+    <Note/>
     </div>
   );
 };
